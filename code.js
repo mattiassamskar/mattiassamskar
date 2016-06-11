ec0a499fc1deaeda75ef05b4a2ebc49f746dd446
Mattias Sämskar <mattias.samskar@gmail.com>
2016-06-11 16:22:22 +0200

    Cleanup code

--- code.js
+++ code.js
@@ -8,6 +8,7 @@ animate();
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
+    camera.position.z = 1000;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
@@ -15,9 +16,7 @@ function init() {
     window.addEventListener('resize', callback, false);
 
     initStars();
-    // initImage();
     initBox();
-    camera.position.z = 1000;
 }
 
 function animate() {
@@ -29,19 +28,13 @@ function animate() {
 
 function initStars() {
     for (i = -2000; i < 1000; i += 20) {
-        var star = createStarMesh(i);
+        var geometry = new THREE.CircleGeometry(3);
+        var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+        var star = new THREE.Mesh(geometry, material);
         setMeshStartPosition(star, i);
         stars.push(star);
         scene.add(star);
     }
-}
-
-function initImage() {
-    var map = new THREE.TextureLoader().load("mattias.png");
-    var material = new THREE.SpriteMaterial({ map: map });
-    var mesh = new THREE.Sprite(material);
-    mesh.position.set(0, 0, 996);
-    scene.add(mesh);
 }
 
 function initBox() {
@@ -54,26 +47,18 @@ function initBox() {
 }
 
 function animateStars() {
-    for (i = 0; i < stars.length; i++) {
-        var star = stars[i];
+    stars.forEach(function (star) {
         star.position.z += 15;
         if (star.position.z > 1000) {
             setMeshStartPosition(star, -2000);
         }
-    }
+    });
 }
 
 function animateBox() {
     box.rotation.x += 0.02;
     box.rotation.y += 0.01;
     box.rotation.z += 0.03;
-}
-
-function createStarMesh() {
-    var geometry = new THREE.CircleGeometry(3);
-    var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
-    var mesh = new THREE.Mesh(geometry, material);
-    return mesh;
 }
 
 function setMeshStartPosition(mesh, z) {
