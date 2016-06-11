93ef685f05161f7079475fdc88b7cfe49c0215a3
Mattias Sämskar <mattias.samskar@gmail.com>
2016-06-11 17:32:57 +0200

    Add rotating earth. Add greeting text.

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 var scene, camera, renderer;
-var stars = [];
-var box;
+var stars = [], box, earth;
+var time = 0;
 
 init();
 animate();
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
     camera.position.z = 1000;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -17,12 +17,14 @@ function init() {
 
     initStars();
     initBox();
+    initEarth();
 }
 
 function animate() {
     requestAnimationFrame(animate);
     animateStars();
     animateBox();
+    animateEarth();
     renderer.render(scene, camera);
 }
 
@@ -38,12 +40,20 @@ function initStars() {
 }
 
 function initBox() {
-    var geometry = new THREE.BoxGeometry(100, 100, 100);
+    var geometry = new THREE.BoxGeometry(60, 60, 60);
     var map = new THREE.TextureLoader().load("mattias.png");
     var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: map });
     box = new THREE.Mesh(geometry, material);
-    box.position.set(0, 0, 700);
+    box.position.set(0, 0, 0);
     scene.add(box);
+}
+
+function initEarth() {
+    var geometry = new THREE.SphereGeometry(30, 32, 32);
+    var map = new THREE.TextureLoader().load("earth.jpg");
+    var material = new THREE.MeshBasicMaterial({ color: 0xcccccc, map: map });
+    earth = new THREE.Mesh(geometry, material);
+    scene.add(earth);
 }
 
 function animateStars() {
@@ -56,9 +66,16 @@ function animateStars() {
 }
 
 function animateBox() {
-    box.rotation.x += 0.02;
-    box.rotation.y += 0.01;
-    box.rotation.z += 0.03;
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.005;
+    box.rotation.z += 0.015;
+}
+
+function animateEarth() {
+    time = time + 10;
+    var e_angle = time * 0.001;
+    earth.rotation.y += 0.01;
+    earth.position.set(0.7 * window.innerWidth * Math.cos(e_angle) / 2, 0.7 * window.innerHeight * Math.sin(e_angle) / 2, 0);
 }
 
 function setMeshStartPosition(mesh, z) {
