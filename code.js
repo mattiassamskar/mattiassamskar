986595a144230c3fd194bc090519d0cbb1c36837
Familjen Sämskar <Familjen Smskar>
2017-02-13 13:24:55 +0100

    Add tilt. Add instagram.

--- code.js
+++ code.js
@@ -7,30 +7,42 @@ var planets = [
         image: 'linkedin.png',
         url: 'https://se.linkedin.com/in/mattiassamskar',
         distance: 0.3,
-        speed: 0.013,
-        size: 25
+        speed: 0.007,
+        size: 23,
+        tilt: -0.10
     },
     {
         image: 'github.jpg',
         url: 'https://github.com/mattiassamskar',
-        distance: 0.25,
-        speed: 0.01,
-        size: 27
+        distance: 0.18,
+        speed: 0.006,
+        size: 25,
+        tilt: 0.06
     },
     {
         image: 'twitter.jpg',
         url: 'https://twitter.com/mattiassamskar',
-        distance: 0.4,
-        speed: 0.015,
-        size: 20
+        distance: 0.27,
+        speed: 0.009,
+        size: 20,
+        tilt: -0.15
     },
     {
         image: 'facebook.png',
         url: 'https://facebook.com/mattias.samskar',
-        distance: 0.55,
-        speed: 0.007,
-        size: 15
-    }
+        distance: 0.33,
+        speed: 0.011,
+        size: 15,
+        tilt: 0.0001
+    },
+    {
+        image: 'instagram.jpg',
+        url: 'https://www.instagram.com/mattiassamskar/',
+        distance: 0.13,
+        speed: 0.015,
+        size: 16,
+        tilt: 0.13
+    },
 ];
 
 function initStars() {
@@ -46,7 +58,7 @@ function initStars() {
 }
 
 function initBox() {
-    var geometry = new THREE.BoxGeometry(70, 70, 70);
+    var geometry = new THREE.BoxGeometry(60, 60, 60);
     var map = new THREE.TextureLoader().load("mattias.jpg");
     var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: map });
     box = new THREE.Mesh(geometry, material);
@@ -94,10 +106,10 @@ function animatePlanets() {
     var windowSize = Math.min(window.innerWidth, window.innerHeight);
     planets.forEach(function (planet) {
         var x = planet.distance * windowSize * Math.cos(planet.e_angle);
-        var y = planet.distance * windowSize * Math.sin(planet.e_angle);
-        var z = 100 * Math.cos(planet.e_angle);
+        var y = planet.tilt * windowSize * Math.sin(planet.e_angle);
+        var z = planet.distance * 2 * windowSize * Math.sin(planet.e_angle);
         planet.mesh.position.set(x, y, z);
-        planet.mesh.rotation.y += 0.03;
+        planet.mesh.rotation.y += planet.speed;
         planet.e_angle += planet.speed;
     });
 }
@@ -130,7 +142,7 @@ function onDocumentTouchStart(event) {
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
     camera.position.z = 1000;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
