6bf228c0cf405bbc3c12c6dd7690d91a63bc24db
Mattias Sämskar <mattias.samskar@gmail.com>
2017-02-13 12:28:05 +0100

    Add links to github, twitter, facebook and linkedin

--- code.js
+++ code.js
@@ -1,39 +1,40 @@
-var scene, camera, renderer, projector;
-var stars = [], box, earth;
-var time = 0;
+'use strict';
 
+var scene, camera, renderer, projector, raycaster, mouse, stars = [], box, planet, time = 0;
 
-init();
-animate();
-
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
-    camera.position.z = 1000;
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    raycaster = new THREE.Raycaster();
-    mouse = new THREE.Vector2();
-    document.body.appendChild(renderer.domElement);
-
-    window.addEventListener('resize', callback, false);
-    document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('touchstart', onDocumentTouchStart, false);
-
-    initStars();
-    initBox();
-    initEarth();
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    animateStars();
-    animateBox();
-    animateEarth();
-    renderer.render(scene, camera);
-}
+var planets = [
+    {
+        image: 'linkedin.png',
+        url: 'https://se.linkedin.com/in/mattiassamskar',
+        distance: 0.3,
+        speed: 0.013,
+        size: 25
+    },
+    {
+        image: 'github.jpg',
+        url: 'https://github.com/mattiassamskar',
+        distance: 0.25,
+        speed: 0.01,
+        size: 27
+    },
+    {
+        image: 'twitter.jpg',
+        url: 'https://twitter.com/mattiassamskar',
+        distance: 0.4,
+        speed: 0.015,
+        size: 20
+    },
+    {
+        image: 'facebook.png',
+        url: 'https://facebook.com/mattias.samskar',
+        distance: 0.55,
+        speed: 0.007,
+        size: 15
+    }
+];
 
 function initStars() {
+    var i;
     for (i = -2000; i < 1000; i += 20) {
         var geometry = new THREE.CircleGeometry(3);
         var material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
@@ -45,20 +46,33 @@ function initStars() {
 }
 
 function initBox() {
-    var geometry = new THREE.BoxGeometry(60, 60, 60);
-    var map = new THREE.TextureLoader().load("mattias.png");
+    var geometry = new THREE.BoxGeometry(70, 70, 70);
+    var map = new THREE.TextureLoader().load("mattias.jpg");
     var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: map });
     box = new THREE.Mesh(geometry, material);
     box.position.set(0, 0, 0);
     scene.add(box);
 }
 
-function initEarth() {
-    var geometry = new THREE.SphereGeometry(30, 32, 32);
-    var map = new THREE.TextureLoader().load("linkedin.png");
-    var material = new THREE.MeshBasicMaterial({ color: 0xcccccc, map: map });
-    earth = new THREE.Mesh(geometry, material);
-    scene.add(earth);
+function initPlanets() {
+    planets.forEach(function (planet) {
+        var geometry = new THREE.SphereGeometry(planet.size, 50, 50);
+        var map = new THREE.TextureLoader().load(planet.image);
+        var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: map });
+        planet.mesh = new THREE.Mesh(geometry, material);
+        planet.mesh.userData = planet.url;
+        planet.e_angle = Math.random() * 10;
+        scene.add(planet.mesh);
+    });
+}
+
+function setMeshStartPosition(mesh, z) {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    mesh.position.x = 3 * (Math.random() * width - width / 2);
+    mesh.position.y = 3 * (Math.random() * height - height / 2);
+    mesh.position.z = z;
 }
 
 function animateStars() {
@@ -76,28 +90,36 @@ function animateBox() {
     box.rotation.z += 0.015;
 }
 
-function animateEarth() {
-    time = time + 10;
-    var e_angle = time * 0.001;
-    earth.rotation.y += 0.03;
-    earth.position.set(0.7 * window.innerWidth * Math.cos(e_angle) / 2, 0.7 * window.innerHeight * Math.sin(e_angle) / 2, 0);
+function animatePlanets() {
+    var windowSize = Math.min(window.innerWidth, window.innerHeight);
+    planets.forEach(function (planet) {
+        var x = planet.distance * windowSize * Math.cos(planet.e_angle);
+        var y = planet.distance * windowSize * Math.sin(planet.e_angle);
+        var z = 100 * Math.cos(planet.e_angle);
+        planet.mesh.position.set(x, y, z);
+        planet.mesh.rotation.y += 0.03;
+        planet.e_angle += planet.speed;
+    });
 }
 
-function setMeshStartPosition(mesh, z) {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-
-    mesh.position.x = 3 * (Math.random() * width - width / 2);
-    mesh.position.y = 3 * (Math.random() * height - height / 2);
-    mesh.position.z = z;
-}
-
-function callback() {
+function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 }
 
+function onDocumentMouseDown(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(planets.map(function (planet) {
+        return planet.mesh;
+    }));
+    if (intersects.length > 0) {
+        window.open(intersects[0].object.userData);
+    }
+}
 
 function onDocumentTouchStart(event) {
     event.preventDefault();
@@ -106,13 +128,32 @@ function onDocumentTouchStart(event) {
     onDocumentMouseDown(event);
 }
 
-function onDocumentMouseDown(event) {
-    event.preventDefault();
-    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-    mouse.y = - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects([earth]);
-    if (intersects.length > 0) {
-        window.open('https://se.linkedin.com/in/mattiassamskar');
-    }
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
+    camera.position.z = 1000;
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+    document.body.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    document.addEventListener('touchstart', onDocumentTouchStart, false);
+
+    initStars();
+    initBox();
+    initPlanets();
 }
+
+function animate() {
+    requestAnimationFrame(animate);
+    animateStars();
+    animateBox();
+    animatePlanets();
+    renderer.render(scene, camera);
+}
+
+init();
+animate();
