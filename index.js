2176a8477ca0cd0fa958e91776b1d67b6f94e304
Mattias Sämskar <mattias.samskar@gmail.com>
2021-09-08 21:05:41 +0200

    Remove old links

--- index.js
+++ index.js
@@ -7,54 +7,34 @@ var box;
 var stars = [];
 var planets = [
   {
-    image: './img/linkedin.png',
-    url: 'https://se.linkedin.com/in/mattiassamskar',
+    image: "./img/linkedin.png",
+    url: "https://se.linkedin.com/in/mattiassamskar",
     distance: 500,
     speed: 0.007,
     size: 23,
     tilt: -1,
     angle: 0,
-    mesh: null
+    mesh: null,
   },
   {
-    image: './img/github.jpg',
-    url: 'https://github.com/mattiassamskar',
+    image: "./img/github.jpg",
+    url: "https://github.com/mattiassamskar",
     distance: 360,
     speed: 0.008,
     size: 25,
     tilt: 1,
     angle: 0,
-    mesh: null
+    mesh: null,
   },
   {
-    image: './img/twitter.jpg',
-    url: 'https://twitter.com/mattiassamskar',
-    distance: 420,
-    speed: 0.009,
-    size: 20,
-    tilt: -1,
-    angle: 0,
-    mesh: null
-  },
-  {
-    image: './img/facebook.png',
-    url: 'https://facebook.com/mattias.samskar',
+    image: "./img/mail.jpg",
+    url: "mailto:mattias.samskar@gmail.com",
     distance: 400,
     speed: 0.011,
-    size: 15,
+    size: 30,
     tilt: 1,
     angle: 0,
-    mesh: null
-  },
-  {
-    image: './img/instagram.jpg',
-    url: 'https://www.instagram.com/mattiassamskar/',
-    distance: 260,
-    speed: 0.015,
-    size: 16,
-    tilt: -1,
-    angle: 0,
-    mesh: null
+    mesh: null,
   },
 ];
 
@@ -116,7 +96,7 @@ var animateBox = function () {
 var animatePlanets = function () {
   planets.forEach(function (planet) {
     var x = planet.distance * Math.cos(planet.angle * planet.tilt);
-    var y = planet.distance * Math.sin(planet.angle * planet.tilt) / 3;
+    var y = (planet.distance * Math.sin(planet.angle * planet.tilt)) / 3;
     var z = planet.distance * Math.sin(planet.angle);
     planet.mesh.position.set(x, y, z);
     planet.mesh.rotation.y += planet.speed;
@@ -135,7 +115,11 @@ var onDocumentMouseDown = function (event) {
   mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
   mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
   raycaster.setFromCamera(mouse, camera);
-  var intersects = raycaster.intersectObjects(planets.map(function (planet) { return planet.mesh; }));
+  var intersects = raycaster.intersectObjects(
+    planets.map(function (planet) {
+      return planet.mesh;
+    })
+  );
   if (intersects.length > 0) {
     window.open(intersects[0].object.userData);
   }
@@ -150,16 +134,21 @@ var onDocumentTouchStart = function (event) {
 
 var init = function () {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+  camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
   camera.position.z = 1000;
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   raycaster = new THREE.Raycaster();
   mouse = new THREE.Vector2();
   document.body.appendChild(renderer.domElement);
-  window.addEventListener('resize', onWindowResize, false);
-  document.addEventListener('mousedown', onDocumentMouseDown, false);
-  document.addEventListener('touchstart', onDocumentTouchStart, false);
+  window.addEventListener("resize", onWindowResize, false);
+  document.addEventListener("mousedown", onDocumentMouseDown, false);
+  document.addEventListener("touchstart", onDocumentTouchStart, false);
   initStars();
   initBox();
   initPlanets();
